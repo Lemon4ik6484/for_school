627da08259fdd80f827be60b35fff1d09b938238
Lemonnik6484 <LemonGamer6484@gmail.com>
2023-10-03 07:42:58 +0200

    update

---
+++ Custom/lessons.js
@@ -0,0 +1,92 @@
+export const lessons = [
+  {
+    name: "physics",
+    title: "Фізика",
+    link: "https://us04web.zoom.us/j/71160338554?pwd=cuRM5DuC4C5wbYVRs4U02ELBCngCLA.1",
+  },
+  {
+    name: "algebra",
+    title: "Алгебра",
+    link: "https://us04web.zoom.us/j/78388130486?pwd=cnTD7LvyaPHxKx7AmE7R7ox5kPetX3.1",
+  },
+  {
+    name: "geometry",
+    title: "Геометрія",
+    link: "https://us04web.zoom.us/j/78388130486?pwd=cnTD7LvyaPHxKx7AmE7R7ox5kPetX3.1",
+  },
+  {
+    name: "computerscience",
+    title: "Інформатика",
+    link: "https://us04web.zoom.us/j/8540626601?pwd=UkFyQWpFWW1GU0xSRnlteEMwSUNWdz09",
+  },
+  {
+    name: "literature",
+    title: "Література",
+    link: "https://us04web.zoom.us/j/77586110056?pwd=E2pJOVQeQPUtJSJgPAHeRki5YzdBAg.1",
+  },
+  {
+    name: "ukrliterature",
+    title: "Українська література",
+    link: "https://us05web.zoom.us/j/8525252562?pwd=UytGVTVLV2RFWnh6bGU4RTFGU1FUdz09",
+  },
+  {
+    name: "ukrlanguage",
+    title: "Українська мова",
+    link: "https://us05web.zoom.us/j/8525252562?pwd=UytGVTVLV2RFWnh6bGU4RTFGU1FUdz09",
+  },
+  {
+    name: "english",
+    title: "Англійська мова",
+    link: "https://us04web.zoom.us/j/5626783573?pwd=WDRJWXQyekNVL1k1S3dsdSs5WEVJZz09",
+  },
+  {
+    name: "history",
+    title: "Всесвітня історія",
+    link: "https://us04web.zoom.us/j/9228531715?pwd=ZjBrVXFZRHQyU0puS1RGOGxzYUNLdz09",
+  },
+  {
+    name: "ukrhistory",
+    title: "Історія України",
+    link: "https://us04web.zoom.us/j/9228531715?pwd=ZjBrVXFZRHQyU0puS1RGOGxzYUNLdz09",
+  },
+  {
+    name: "kharkiv",
+    title: "Харківщино-знавство",
+    link: "https://us04web.zoom.us/j/9228531715?pwd=ZjBrVXFZRHQyU0puS1RGOGxzYUNLdz09",
+  },
+  {
+    name: "chemistry",
+    title: "Хімія",
+    link: "https://us04web.zoom.us/j/8717700620?pwd=eUxIcW1rZjRXZ21va0ZVQnlWVzFpUT09",
+  },
+  {
+    name: "biology",
+    title: "Біологія",
+    link: "https://us05web.zoom.us/j/5282604925?pwd=OEViMkxNTTRLb3p4MnpUN0NKck5HZz09",
+  },
+  {
+    name: "geography",
+    title: "Географія",
+    link: "https://us04web.zoom.us/j/75463486414?pwd=77bNt9iuKZ7FEF5RnDqBd6ZJW6itlK.1",
+  },
+  {
+    name: "health",
+    title: "Основи здоров'я",
+    link: "https://us05web.zoom.us/j/5282604925?pwd=OEViMkxNTTRLb3p4MnpUN0NKck5HZz09",
+  },
+  {
+    name: "physicaleducation",
+    title: "Фізична культура",
+    link: "https://us04web.zoom.us/j/4786617178?pwd=UUx2cGc0eENDdnhkeXJHc0pTcmVCQT09",
+  },
+  {
+    name: "arts",
+    title: "Мистецтво",
+    link: "https://us05web.zoom.us/j/83512290078?pwd=fclEKRAuLF7DiJgdi2sjWoW6FaUmV1.1",
+  },
+  {
+    name: "labors",
+    title: "Трудове навчання",
+    link: "https://us04web.zoom.us/j/78388130486?pwd=cnTD7LvyaPHxKx7AmE7R7ox5kPetX3.1",
+  },
+];
